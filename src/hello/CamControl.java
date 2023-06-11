4ebfa1f482c246f53b1d437ef43b3ea2f3246a57
nslookupalexios <a.guarini7@studenti.unisa.it>
2023-06-11 22:37:30 +0200

    Refactoring mirato ad aumentare l'efficienza.

--- src/hello/CamControl.java
+++ src/hello/CamControl.java
@@ -10,28 +10,28 @@ import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 
-/**
- *
- * @author foggia
- */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class CamControl extends AbstractControl implements AnalogListener {
-    //Any local variables should be encapsulated by getters/setters so they
-    //appear in the SDK properties window and can be edited.
-    //Right-click a local variable to encapsulate it with getters and setters.
-    
-    private Camera camera;
-    private Vector3f offset;
-    private Vector3f pos;
-    private Quaternion rotation;
-    private static final float OMEGA=1.0f;
-    
+
+    private final Camera camera;
+    private static final Vector3f offset = new Vector3f(0f, 1.5f, 5.5f);
+    private static final Vector3f pos = new Vector3f();
+    private static final Quaternion rotation = new Quaternion();
+    private static final float OMEGA=2.0f;
+
+    private static final Map<String, BiConsumer<Float, Quaternion>> actionsMap = new HashMap<>() {{
+        put("CamLeft",  (value, rotation) -> rotation.fromAngles(0f,  OMEGA*value, 0f).multLocal(offset));
+        put("CamRight", (value, rotation) -> rotation.fromAngles(0f, -OMEGA*value, 0f).multLocal(offset));
+        put("CamUp",    (value, rotation) -> rotation.fromAngles(-OMEGA*value, 0f, 0f).multLocal(offset));
+        put("CamDown",  (value, rotation) -> rotation.fromAngles(OMEGA*value, 0f, 0f).multLocal(offset));
+    }};
+
     public CamControl(Camera camera, InputManager inputManager) {
         this.camera=camera;
-        offset=new Vector3f(0f, 1.5f, 5.5f);
-        pos=new Vector3f();
-        inputManager.addListener(this,
-                "CamLeft", "CamRight");
-        rotation=new Quaternion();
+        inputManager.addListener(this, "CamLeft", "CamRight", "CamUp", "CamDown");
     }
 
     @Override
@@ -45,7 +45,7 @@ public class CamControl extends AbstractControl implements AnalogListener {
         pos.y += 1.0f;
         camera.lookAt(pos, Vector3f.UNIT_Y);
     }
-    
+
     @Override
     protected void controlRender(RenderManager rm, ViewPort vp) {
         //Only needed for rendering-related operations,
@@ -54,13 +54,9 @@ public class CamControl extends AbstractControl implements AnalogListener {
 
     @Override
     public void onAnalog(String input, float value, float tpf) {
-        if (input.equals("CamLeft")) {
-            rotation.fromAngles(0f, -OMEGA*value, 0f);
-            rotation.multLocal(offset);
-        } else if (input.equals("CamRight")) {
-            rotation.fromAngles(0f, OMEGA*value, 0f);
-            rotation.multLocal(offset);            
+        BiConsumer<Float, Quaternion> action = actionsMap.get(input);
+        if(action != null) {
+            action.accept(value, rotation);
         }
     }
-    
 }
--- src/hello/MotionControl.java
+++ src/hello/MotionControl.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JME3 Classes/Control.java to edit this template
- */
 package hello;
 
 import com.jme3.anim.AnimComposer;
 import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.export.InputCapsule;
-import com.jme3.export.JmeExporter;
-import com.jme3.export.JmeImporter;
-import com.jme3.export.OutputCapsule;
 import com.jme3.input.InputManager;
 import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.AnalogListener;
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
@@ -21,32 +12,30 @@ import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
-import com.jme3.scene.control.Control;
-import java.io.IOException;
 
-/**
- *
- * @author foggia
- */
 public class MotionControl extends AbstractControl implements ActionListener {
-    //Any local variables should be encapsulated by getters/setters so they
-    //appear in the SDK properties window and can be edited.
-    //Right-click a local variable to encapsulate it with getters and setters.
-    private static final float OMEGA=2*FastMath.PI/10;
-    private static final float VELOCITY=3.0f;
-    private static final float ANIM_SPEED_RATIO=0.4f;
-    private static final float ANGLE_CORRECTION_FACTOR=1.0f;
-    private Vector3f s=new Vector3f();
-    private Vector3f w=new Vector3f();
-    private AnimComposer animComposer=null;
-    private boolean moveForward=false, moveBackward=false,
-            rotateLeft=false, rotateRight=false;
-    private String currentAction=null;
-    private Quaternion quaternion=new Quaternion();
-    private float[] angles=new float[3];
-    
-    public MotionControl(InputManager inputManager) {
-        inputManager.addListener(this, "Forward", 
+
+    private static final float OMEGA = 2 * FastMath.PI / 10;
+    private static final float VELOCITY = 8.0f;
+    private static final float ANIM_SPEED_RATIO = 0.4f;
+    private static final float ANGLE_CORRECTION_FACTOR = 1.0f;
+
+    private final Vector3f s = new Vector3f();
+    private final Vector3f w = new Vector3f();
+    private final RigidBodyControl rbc;
+    private final Quaternion quaternion = new Quaternion();
+    private final float[] angles = new float[3];
+
+    private AnimComposer animComposer;
+    private boolean moveForward = false, moveBackward = false,
+            rotateLeft = false, rotateRight = false;
+    private String currentAction = null;
+
+    public MotionControl(InputManager inputManager, RigidBodyControl rbc) {
+        this.rbc = rbc;
+        this.animComposer = findAnimComposer(rbc.getSpatial());
+
+        inputManager.addListener(this, "Forward",
                 "Backward",
                 "RotateLeft", "RotateRight");
     }
@@ -54,60 +43,58 @@ public class MotionControl extends AbstractControl implements ActionListener {
     @Override
     protected void controlUpdate(float tpf) {
         if (moveForward) {
-            setAction("run", VELOCITY*ANIM_SPEED_RATIO);
+            setAction("run", VELOCITY * ANIM_SPEED_RATIO);
             forward(1.0f);
         } else if (moveBackward) {
-            setAction("run", -0.5f*VELOCITY*ANIM_SPEED_RATIO);
+            setAction("run", -0.5f * VELOCITY * ANIM_SPEED_RATIO);
             forward(-0.5f);
-        }  else {
+        } else {
             setAction("idle", 1.0f);
         }
-        
-        if (rotateLeft) 
+
+        if (rotateLeft) {
             rotate(1.0f);
-        else if (rotateRight)
+        } else if (rotateRight) {
             rotate(-1.0f);
-        
+        }
+
         straightenUp();
     }
-    
+
     private void forward(float value) {
-        float d=VELOCITY*value;
+        float d = VELOCITY * value;
         s.set(0f, 0f, d);
-        Spatial dino=getSpatial();
-        
-        Quaternion localRotation=dino.getLocalRotation();
+        Spatial dino = getSpatial();
+        Quaternion localRotation = dino.getLocalRotation();
         localRotation.mult(s, w);
-        
-        RigidBodyControl rbc=dino.getControl(RigidBodyControl.class);
-        rbc.getLinearVelocity(s);
+
+        this.rbc.getLinearVelocity(s);
         w.y = s.y;
-        
-        rbc.setLinearVelocity(w);
+
+        this.rbc.setLinearVelocity(w);
     }
-    
+
     private void setAction(String action, float speed) {
-        if (action.equals(currentAction))
+        if (action.equals(currentAction)) {
             return;
-        currentAction=action;
-        AnimComposer a=getAnimComposer();
-        if (a!=null) {
-            
+        }
+        currentAction = action;
+        AnimComposer a = getAnimComposer();
+        if (a != null) {
+
             a.setCurrentAction(action);
             a.setGlobalSpeed(speed);
         }
     }
-    
+
     private void rotate(float value) {
-        float ang=OMEGA*value;
-        Spatial dino=getSpatial();
-        RigidBodyControl rbc=dino.getControl(RigidBodyControl.class);
-        rbc.getAngularVelocity(s);
-        s.y=ang;
-        rbc.setAngularVelocity(s);
-        
+        float ang = OMEGA * value;
+        this.rbc.getAngularVelocity(s);
+        s.y = ang;
+        this.rbc.setAngularVelocity(s);
+
     }
-    
+
     @Override
     protected void controlRender(RenderManager rm, ViewPort vp) {
         //Only needed for rendering-related operations,
@@ -116,22 +103,21 @@ public class MotionControl extends AbstractControl implements ActionListener {
 
     @Override
     public void onAction(String input, boolean active, float tpf) {
-        if (!isEnabled())
+        if (!isEnabled()) {
             return;
-        if (input.equals("Forward")) 
-            moveForward=active;
-        else if (input.equals("Backward"))
-            moveBackward=active;
-        else if (input.equals("RotateLeft"))
-            rotateLeft=active;
-        else if (input.equals("RotateRight"))
-            rotateRight=active;
-    }
-    
-    private AnimComposer getAnimComposer() {
-        if (animComposer==null) {
-            animComposer=findAnimComposer(getSpatial());
         }
+        if (input.equals("Forward")) {
+            moveForward = active;
+        } else if (input.equals("Backward")) {
+            moveBackward = active;
+        } else if (input.equals("RotateLeft")) {
+            rotateLeft = active;
+        } else if (input.equals("RotateRight")) {
+            rotateRight = active;
+        }
+    }
+
+    private AnimComposer getAnimComposer() {
         return animComposer;
     }
 
@@ -151,39 +137,38 @@ public class MotionControl extends AbstractControl implements ActionListener {
         }
         return null;
     }
-    
-    
+
     @Override
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
-        moveForward=false;
-        moveBackward=false;
-        rotateLeft=false;
-        rotateRight=false;
+        moveForward = false;
+        moveBackward = false;
+        rotateLeft = false;
+        rotateRight = false;
         setAction("idle", 1.0f);
-        
+
     }
-    
+
     private void straightenUp() {
-        Spatial dino=getSpatial();
-        RigidBodyControl rbc=dino.getControl(RigidBodyControl.class);
-        rbc.getPhysicsRotation(quaternion);
+        this.rbc.getPhysicsRotation(quaternion);
         quaternion.toAngles(angles);
-        float ang_x=normalize(angles[0]);
-        float ang_z=normalize(angles[2]);
-        float mass=rbc.getMass();
-        w.set(-ang_x*mass*ANGLE_CORRECTION_FACTOR,
-              0.0f,
-              -ang_z*mass*ANGLE_CORRECTION_FACTOR);
+        float ang_x = normalize(angles[0]);
+        float ang_z = normalize(angles[2]);
+        float mass = this.rbc.getMass();
+        w.set(-ang_x * mass * ANGLE_CORRECTION_FACTOR,
+                0.0f,
+                -ang_z * mass * ANGLE_CORRECTION_FACTOR);
         rbc.applyTorque(w);
     }
-    
+
     private float normalize(float ang) {
-        while (ang<-FastMath.PI)
-            ang += 2.0f*FastMath.PI;
-        while (ang>FastMath.PI)
-            ang -= 2.0f*FastMath.PI;
+        while (ang < -FastMath.PI) {
+            ang += 2.0f * FastMath.PI;
+        }
+        while (ang > FastMath.PI) {
+            ang -= 2.0f * FastMath.PI;
+        }
         return ang;
     }
-    
+
 }
